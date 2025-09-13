3fcca789a7fe0a725807370d7a3c21340053b382
DeepakAC3 <deepakpradip29@gmail.com>
2025-09-13 11:50:43 +0530

    track nvim

---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  },
+  config = function()
+    require("chatgpt").setup({
+      api_key_cmd="echo 'OPENAI_API_KEY'"
+    })
+    end,
+  -- test new blink
+  { import = "nvchad.blink.lazyspec" },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+}
